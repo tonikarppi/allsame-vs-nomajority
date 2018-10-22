1b2da068bc43ef4074dc26d3985e88de25f53de8
Toni Karppi <karppitoni@gmail.com>
2018-10-23 01:35:02 +0200

    Remove commented out code

--- always-correct.py
+++ always-correct.py
@@ -18,17 +18,3 @@ num_list_str = stdin.readline()
 num_list = [int(s) for s in num_list_str.split(' ')]
 result = all_same_or_no_majority(num_list)
 print(result)
-
-# A1 = [1, 1, 1, 1, 1]
-# A2 = [1, 1, 1, 1]
-# A3 = [1, 1, 2, 2]
-# A4 = [1, 2, 3, 4]
-# A5 = [1, 1, 5, 1, 3, 4]
-# A6 = [1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 89, 1, 49, 1, 1, 1, 58, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-#       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 12, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 79, 1, 1, 1, 69, 74, 1, 87, 1, 1, 1]
-
-# inputs = [A1, A2, A3, A4, A5, A6]
-
-# for A in inputs:
-#     result1 = all_same_or_no_majority(A)
-#     print(result1)
